54e54fce0075005590c05a6c6e4ace2cfc3b9a6d
aemilani <aealieftekhary@gmail.com>
2025-04-07 11:37:19 +0200

    Refactored the trading code

--- round_0.py
+++ round_0.py
@@ -1,18 +1,63 @@
-import jsonpickle
 import numpy as np
-from datamodel import OrderDepth, UserId, TradingState, Order
+from datamodel import OrderDepth, TradingState, Order
 from typing import List
+
+
+def trade_resin(curr_position: int) -> List[Order]:
+    fair_value = 10000
+
+    max_position = 50
+    max_buy_size = min(max_position, max_position - curr_position)
+    max_sell_size = max(-max_position, -max_position - curr_position)
+
+    thr_l = fair_value - 2
+    thr_h = fair_value + 2
+    buy_price = thr_l
+    sell_price = thr_h
+
+    resin_orders = []
+    if max_buy_size > 0:
+        print(f'BUY: {max_buy_size} @ {buy_price}')
+        resin_orders.append(Order('RAINFOREST_RESIN', buy_price, max_buy_size))
+    if max_sell_size < 0:
+        print(f'SELL: {max_sell_size} @ {sell_price}')
+        resin_orders.append(Order('RAINFOREST_RESIN', sell_price, max_sell_size))
+
+    return resin_orders
+
+
+def trade_kelp(curr_position: int, order_depth: OrderDepth) -> List[Order]:
+    max_position = 50
+    max_buy_size = min(max_position, max_position - curr_position)
+    max_sell_size = max(-max_position, -max_position - curr_position)
+
+    buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+    sell_orders = sorted(order_depth.sell_orders.items())
+
+    popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+    popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+
+    fair_value = round((popular_buy_price + popular_sell_price) / 2)
+    print(f'KELP fair value: {fair_value}')
+
+    kelp_orders = []
+    if max_buy_size > 0:
+        buy_price = int(np.floor(fair_value)) - 1
+        print(f'BUY: {max_buy_size} @ {buy_price}')
+        kelp_orders.append(Order('KELP', buy_price, max_buy_size))
+    if max_sell_size < 0:
+        sell_price = int(np.ceil(fair_value)) + 1
+        print(f'SELL: {max_sell_size} @ {sell_price}')
+        kelp_orders.append(Order('KELP', sell_price, max_sell_size))
+
+    return kelp_orders
 
 
 class Trader:
     def run(self, state: TradingState):
-
         result = {}
         for product in state.order_depths:
-
             order_depth: OrderDepth = state.order_depths[product]
-            print(f'{product} bids: {order_depth.buy_orders}')
-            print(f'{product} asks: {order_depth.sell_orders}')
 
             if product in state.position:
                 if state.position[product]:
@@ -24,57 +69,13 @@ class Trader:
             print(f'{product} position: {curr_position}')
 
             orders: List[Order] = []
-
-            # RAINFOREST_RESIN ----------------------------------------------------------------------------------------
             if product == 'RAINFOREST_RESIN':
-                fair_value = 10000
-                max_position = 50
-
-                max_buy_size = min(max_position, max_position - curr_position)
-                max_sell_size = max(-max_position, -max_position - curr_position)
-
-                thr_l = fair_value - 2
-                thr_h = fair_value + 2
-                buy_price = thr_l
-                sell_price = thr_h
-
-                if max_buy_size > 0:
-                    print(f'BUY: {max_buy_size} @ {buy_price}')
-                    orders.append(Order(product, buy_price, max_buy_size))
-
-                if max_sell_size < 0:
-                    print(f'SELL: {max_sell_size} @ {sell_price}')
-                    orders.append(Order(product, sell_price, max_sell_size))
-
-            # KELP ----------------------------------------------------------------------------------------------------
+                orders.extend(trade_resin(curr_position))
             elif product == 'KELP':
-                max_position = 50
-                max_buy_size = min(max_position, max_position - curr_position)
-                max_sell_size = max(-max_position, -max_position - curr_position)
-
-                buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
-                sell_orders = sorted(order_depth.sell_orders.items())
-
-                popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
-                popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
-
-                fair_value = round((popular_buy_price + popular_sell_price) / 2)
-                print(f'{product} fair value: {fair_value}')
-
-                if max_buy_size > 0:
-                    buy_price = int(np.floor(fair_value)) - 1
-                    print(f'BUY: {max_buy_size} @ {buy_price}')
-                    orders.append(Order(product, buy_price, max_buy_size))
-
-                if max_sell_size < 0:
-                    sell_price = int(np.ceil(fair_value)) + 1
-                    print(f'SELL: {max_sell_size} @ {sell_price}')
-                    orders.append(Order(product, sell_price, max_sell_size))
+                orders.extend(trade_kelp(curr_position, order_depth))
 
             result[product] = orders
 
-        traderData = None
-
-        # Sample conversion request. Check more details below.
+        trader_data = None
         conversions = 1
-        return result, conversions, traderData
+        return result, conversions, trader_data
